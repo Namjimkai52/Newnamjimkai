d0cffedbbfb14dd5dffbb21481c5df17b3e416ce
Namjimkai52 <soranathkonde@gmail.com>
2024-12-01 01:04:44 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int NUM = 1; 
+    int counter_even = 0;
+    int counter_odd = 0;
+       while (NUM != 0){
+            cout << "Enter an integer: ";
+            cin >> NUM ;
+       
+      if (NUM != 0){      
+       if(NUM%2 == 0){
+            counter_even = counter_even + 1 ;
+
+        // cout << "#Even numbers = " << NUM1  ;
+       }else {
+            counter_odd = counter_odd + 1 ;
+
+        // cout << "#Odd numbers = "<< NUM2 ;
+       }
+         }
+       }
+         cout << "#Even numbers = " << counter_even << "\n";
+         cout << "#Odd numbers = "<< counter_odd << "\n";
     return 0;
 }
